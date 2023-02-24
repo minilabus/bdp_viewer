71bd4106fd06f6150cdbc2fc4fe90e5bd743315c
frheault <francois.m.rheault@usherbrooke.ca>
2023-02-24 10:55:16 -0500

    working menu and toggle

--- src/index.js
+++ src/index.js
@@ -9,21 +9,14 @@ import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreen
 import vtkXMLPolyDataReader from '@kitware/vtk.js/IO/XML/XMLPolyDataReader';
 import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
 import vtkHttpDataAccessHelper from '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
-import vtkPLYReader from '@kitware/vtk.js/IO/Geometry/PLYReader';
-import vtkTexture from '@kitware/vtk.js/Rendering/Core/Texture';
-import vtkScalarToRGBA from '@kitware/vtk.js/Common/Core/ScalarsToColors';
-import vtkLookupTable from '@kitware/vtk.js/Common/Core/LookupTable';
 
-// const cors = require('cors');
-// const express = require('express');
-// let app = express();
-// app.use(cors());
-// app.options('*', cors());
 
 import controlPanel from './controller.html';
 
 const { fetchBinary } = vtkHttpDataAccessHelper;
 
+// document.getElementById("yourSubTableId").display = "";  //Show the table
+// document.getElementById("yourSubTableId").display = "none";  //Hide the table
 // ----------------------------------------------------------------------------
 // Standard rendering code setup
 // ----------------------------------------------------------------------------
@@ -43,7 +36,6 @@ actor.setMapper(mapper);
 renderer.addActor(actor);
 renderer.resetCamera();
 renderWindow.render();
-var isTextured = true
 // ----------------------------------------------------------------------------
 // Example code
 // ----------------------------------------------------------------------------
@@ -52,36 +44,60 @@ var isTextured = true
 // ----------------------------------------------------------------------------
 
 const BASE_URL = 'https://github.com/minilabus/bdp_data/raw/main/'
-// const TIME_FILES = ['sub-01_epo-01.xml', 'sub-01_epo-02.xml', 'sub-01_epo-03.xml',
-//                     'sub-01_epo-04.xml', 'sub-01_epo-05.xml', 'sub-01_epo-06.xml',
-//                     'sub-01_epo-07.xml', 'sub-01_epo-08.xml', 'sub-01_epo-09.xml',
-//                     'sub-01_epo-10.xml', 'sub-01_epo-11.xml']
 const TIME_FILES = ['sub-01_epo-01', 'sub-01_epo-02']
+const COLORMAP = {
+  0: [255, 255, 255], // WHITE (FFFFFF)
+  1: [142, 250, 0], // AG (8EFA00)
+  2: [255, 251, 0], // FrOrb (FFFB00)
+  3: [1, 25, 196], // FuG (0119C4)
+  4: [255, 212, 121], // IFG (FFD479)
+  5: [255, 138, 216], // IOG (FF8AD8)
+  6: [118, 214, 255], // ITG (76D6FF)
+  7: [118, 183, 196], // LG (76B7C4)
+  8: [255, 147, 0], // MFG (FF9300)
+  9: [255, 64, 255], // MOG (FF40FF)
+  10: [0, 150, 255], // MTG (0096FF)
+  11: [0, 145, 207], // PHG (0091CF)
+  12: [0, 249, 0], // PoCG (00F900)
+  13: [255, 38, 0], // PrCG (FF2600)
+  14: [148, 17, 0], // SFG (941100)
+  15: [212, 251, 121], // SMG (D4FB79)
+  16: [148, 55, 255], // SOG (9437FF)
+  17: [0, 144, 81], // SPG (009051)
+  18: [4, 51, 255], // STG (0433FF)
+  19: [0, 113, 255], // TP (0071FF)
+};
 
-const COLORMAP = [
-  [1, 1, 1, 1],
-  [0.19215686, 0.50980392, 0.74117647, 1.],
-  [0.41960784, 0.68235294, 0.83921569, 1.],
-  [0.61960784, 0.79215686, 0.88235294, 1.],
-  [0.77647059, 0.85882353, 0.9372549, 1.],
-  [0.90196078, 0.33333333, 0.05098039, 1.],
-  [0.99215686, 0.55294118, 0.23529412, 1.],
-  [0.99215686, 0.68235294, 0.41960784, 1.],
-  [0.99215686, 0.81568627, 0.63529412, 1.],
-  [0.19215686, 0.63921569, 0.32941176, 1.],
-  [0.45490196, 0.76862745, 0.4627451, 1.],
-  [0.63137255, 0.85098039, 0.60784314, 1.],
-  [0.78039216, 0.91372549, 0.75294118, 1.],
-  [0.45882353, 0.41960784, 0.69411765, 1.],
-  [0.61960784, 0.60392157, 0.78431373, 1.],
-  [0.7372549, 0.74117647, 0.8627451, 1.],
-  [0.85490196, 0.85490196, 0.92156863, 1.],
-  [0.38823529, 0.38823529, 0.38823529, 1.],
-  [0.58823529, 0.58823529, 0.58823529, 1.],
-  [0.74117647, 0.74117647, 0.74117647, 1.],
-  [0.85098039, 0.85098039, 0.85098039, 1.]]
+const isToggled = {
+  'CorticalToggle': true,
+  'SubCorticalToggle': true,
+  'TractographyToggle': true
+}
 
-console.log(COLORMAP.length)
+const isTextured = {
+  'cor_White': true,
+  'cor_AG': true,
+  'cor_FrOrb': true,
+  'cor_FuG': true,
+  'cor_IFG': true,
+  'cor_IOG': true,
+  'cor_ITG': true,
+  'cor_LG': true,
+  'cor_MFG': true,
+  'cor_MOG': true,
+  'cor_MTG': true,
+  'cor_PHG': true,
+  'cor_PoCG': true,
+  'cor_PrCG': true,
+  'cor_SFG': true,
+  'cor_SMG': true,
+  'cor_SOG': true,
+  'cor_SPG': true,
+  'cor_STG': true,
+  'cor_TP': true,
+}
+const textureNames = Object.keys(isTextured)
+const toggleNames = Object.keys(isToggled)
 
 async function getAnnotData(i) {
   const response = await fetch(BASE_URL + TIME_FILES[i] + '.txt')
@@ -116,39 +132,25 @@ function getDataTimeStep(vtkObj) {
   return null;
 }
 
+function setVisibleDataset(ds) {
+  const oriColors = originalColor[Number(timeslider.value)]
+  const oriAnnot = annotData[Number(timeslider.value)]
+  const rgbaArray = new Uint8Array((oriAnnot.length - 1) * 3);
 
-
-function setVisibleDataset(ds, id) {
-  if (!isTextured) {
-    console.log('not textured')
-    // console.log(annotData[Number(timeslider.value)])
-    // console.log(originalColor[Number(timeslider.value)])
-
-    const oriAnnot = annotData[Number(timeslider.value)]
-    const rgbaArray = new Uint8Array((oriAnnot.length - 1) * 3);
-    // LUT = vtk.vtkLookupTable()
-    // LUT.SetTableRange(255)
-    for (let idx = 0; idx < oriAnnot.length; idx++) {
-
-      // console.log(idx, oriAnnot[idx], c)
-      // console.log(currColor)
-      rgbaArray[(idx * 3)] = 255 * COLORMAP[oriAnnot[idx]][0];
-      rgbaArray[(idx * 3) + 1] = 255 * COLORMAP[oriAnnot[idx]][1];
-      rgbaArray[(idx * 3) + 2] = 255 * COLORMAP[oriAnnot[idx]][2];
+  for (let idx = 0; idx < oriAnnot.length; idx++) {
+    const isCurrOn = isTextured[textureNames[oriAnnot[idx]]]
+    if (!isCurrOn) {
+      rgbaArray[(idx * 3)] = COLORMAP[oriAnnot[idx].toString()][0];
+      rgbaArray[(idx * 3) + 1] = COLORMAP[oriAnnot[idx].toString()][1];
+      rgbaArray[(idx * 3) + 2] = COLORMAP[oriAnnot[idx].toString()][2];
     }
-    ds.getPointData().getArrayByName('RGB').setData(rgbaArray)
-    // console.log(oriColors)
-    // console.log(rgbaArray)
-    // ds.getPointData().getArrayByName('RGB').setData(rgbaArray);
-    // console.log(ds.getPointData().getArrayByName('RGB').getData())
+    else {
+      rgbaArray[(idx * 3)] = oriColors[(idx * 3)];
+      rgbaArray[(idx * 3) + 1] = oriColors[(idx * 3) + 1];
+      rgbaArray[(idx * 3) + 2] = oriColors[(idx * 3) + 2];
+    }
   }
-  else {
-    const oriColors = originalColor[Number(timeslider.value)]
-    ds.getPointData().getArrayByName('RGB').setData(oriColors)
-    console.log('textured')
-  }
-
-
+  ds.getPointData().getArrayByName('RGB').setData(rgbaArray)
   mapper.setInputData(ds);
   renderWindow.render();
 }
@@ -166,21 +168,6 @@ function uiUpdateSlider(max) {
   timeslider.step = 1;
 }
 
-[
-  'Color',
-].forEach((propertyName) => {
-  document.querySelector(`.${propertyName}`).addEventListener('input', (e) => {
-    isTextured = !isTextured
-    console.log(isTextured);
-    const activeDataset = timeSeriesData[Number(timeslider.value)];
-    if (activeDataset) {
-      setVisibleDataset(activeDataset, Number(timeslider.value));
-      timevalue.innerText = getDataTimeStep(activeDataset);
-    }
-    renderWindow.render();
-  });
-});
-
 // -----------------------------------------------------------
 // example code logic
 // -----------------------------------------------------------
@@ -193,12 +180,67 @@ const timevalue = document.querySelector('#timevalue');
 timeslider.addEventListener('input', (e) => {
   const activeDataset = timeSeriesData[Number(e.target.value)];
   if (activeDataset) {
-    setVisibleDataset(activeDataset, Number(e.target.value));
+    setVisibleDataset(activeDataset);
     timevalue.innerText = getDataTimeStep(activeDataset);
   }
 });
 
-console.log('===============================')
+toggleNames.forEach((ToggleButton) => {
+  document.querySelector(`.${ToggleButton}`).addEventListener('click', (e) => {
+    var activeDataset = timeSeriesData[Number(timeslider.value)];
+    
+    const toSet = !toggleNames[ToggleButton]
+    toggleNames[ToggleButton] = toSet
+    var tag = ""
+    if (ToggleButton == 'CorticalToggle') {tag = 'cor_'};
+    if (ToggleButton == 'SubCorticalToggle') {tag = 'sub_'};
+    if (ToggleButton == 'TractographyToggle') {tag = 'tra_'};
+    console.log(tag)
+    for (let idx = 0; idx < textureNames.length; idx++) {
+      const buttonName = textureNames[idx]
+      if (buttonName.slice(0, 4) != tag) {continue}
+
+      isTextured[buttonName] = !toSet
+      document.querySelector(`.${buttonName}`).checked = toSet
+    };
+
+    // Reset Rendering
+    if (timeslider.value == 0) {activeDataset = timeSeriesData[1]}
+    else {activeDataset = timeSeriesData[Number(timeslider.value) - 1]}
+    setVisibleDataset(activeDataset);
+    activeDataset = timeSeriesData[Number(timeslider.value)]
+    setVisibleDataset(activeDataset);
+  })
+  
+});
+
+const representationSelector = document.querySelector('.representations');
+representationSelector.addEventListener('change', (e) => {
+  const newRepValue = Number(e.target.value);
+  const allTables = document.getElementsByTagName("table")
+
+  allTables[1].style.display = "none"
+  allTables[2].style.display = "none"
+  allTables[3].style.display = "none"
+  allTables[newRepValue].style.display = ""
+});
+
+textureNames.forEach((propertyName) => {
+  document.querySelector(`.${propertyName}`).addEventListener('input', (e) => {
+    var activeDataset = timeSeriesData[Number(timeslider.value)];
+
+    // Reset Rendering
+    if (timeslider.value == 0) {activeDataset = timeSeriesData[1]}
+    else {activeDataset = timeSeriesData[Number(timeslider.value) - 1]}
+    setVisibleDataset(activeDataset);
+    activeDataset = timeSeriesData[Number(timeslider.value)]
+    
+    isTextured[propertyName] = !e.target.checked;
+    if (activeDataset) {
+      setVisibleDataset(activeDataset);
+    }
+  });
+});
 
 var originalColor = []
 downloadTimeSeries().then((downloadedData) => {
@@ -216,7 +258,7 @@ downloadTimeSeries().then((downloadedData) => {
   renderer.getActiveCamera().setPosition(0.40, -0., 0.)
   renderer.getActiveCamera().setViewUp(0.1, 0.3, 1.)
 
-  setVisibleDataset(timeSeriesData[0], 0);
+  setVisibleDataset(timeSeriesData[0]);
   timevalue.innerText = getDataTimeStep(timeSeriesData[0]);
 });
 
